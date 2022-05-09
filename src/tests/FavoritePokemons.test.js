2ca87b8d59b3d5a78711c78c7aa452e2bf83ee87
pedronr03 <pedronr0309@gmail.com>
2022-05-09 17:03:55 -0300

    Requisito 3.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste o componente <FavoritePokemons />', () => {
+  test('Teste se mensage aparece', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const phrase = screen.getByText(/no favorite pokemon found/i);
+    expect(phrase).toBeInTheDocument();
+  });
+});
