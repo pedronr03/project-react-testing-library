7ded6987790cf5a425b015e746fd43e7039d3ba1
pedronr03 <pedronr0309@gmail.com>
2022-05-09 15:40:25 -0300

    Requisito 1.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  test('Teste o componente about', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+    userEvent.click(about);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Teste o componente favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(favorites).toBeInTheDocument();
+    userEvent.click(favorites);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('Teste o componente home', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+    userEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+});
