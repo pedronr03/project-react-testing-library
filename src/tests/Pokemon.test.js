68392ad0a3d2fb71fdc0cc73c8e5f446c80f000c
pedronr03 <pedronr0309@gmail.com>
2022-05-10 02:06:36 -0300

    Requisito 6.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,37 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from '../renderWithRouter';
-// import App from '../App';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  const pokemon = pokemons[0];
+  test('Testa se o card é renderizado corretamente.', () => {
+    renderWithRouter(<App />);
+    const weight = pokemon.averageWeight.value;
+    const unit = pokemon.averageWeight.measurementUnit;
+    expect(screen.getByText(pokemon.name)).toBeInTheDocument();
+    expect(screen.getAllByText(pokemon.type)).toHaveLength(2);
+    expect(screen.getByText(`Average weight: ${weight} ${unit}`)).toBeInTheDocument();
+    expect(screen.getByAltText(`${pokemon.name} sprite`))
+      .toHaveAttribute('src', pokemon.image);
+  });
+  test('Testa se o card tem um link de navegação com o id do pokemon.', () => {
+    const { history } = renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(link);
+    expect(history.location.pathname).toBe(`/pokemons/${pokemon.id}`);
+    expect(screen.getByText('Pikachu Details')).toBeInTheDocument();
+  });
+  test('Testa se o pokemon está favoritado.', () => {
+    renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(link);
+    const checkbox = screen.getByLabelText('Pokémon favoritado?');
+    expect(checkbox).toBeInTheDocument();
+    userEvent.click(checkbox);
+    const img = screen.getByRole('img', { name: 'Pikachu is marked as favorite' });
+    expect(img).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
